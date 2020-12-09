0451f9873e6aaba4f809dae9cd5618d4dd5ce1fe
snowtoslow <snowtoslow@gmail.com>
2020-12-09 17:00:23 +0200

    refactor cors;

--- server/app.go
+++ server/app.go
@@ -90,25 +90,6 @@ func (a *App) Run(port string) error {
 
 }
 
-func initClient() (*mongo.Client, error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI(viper.GetString("mongo.uri")))
-	if err != nil {
-		log.Fatalf("Error occured while establishing connection to mongoDB")
-		return nil, err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	return client, nil
-}
-
 func initDB() *mongo.Database {
 	client, err := mongo.NewClient(options.Client().ApplyURI(viper.GetString("mongo.uri")))
 	if err != nil {
